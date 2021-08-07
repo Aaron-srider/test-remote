062d74db22dabe07edbb535bdcd3c44cfa4f80d3
wc <mygit@bistu.cn>
2021-08-07 17:51:38 +0800

    master second commit

--- src/main/java/com/example/testgit/Test.java
+++ src/main/java/com/example/testgit/Test.java
@@ -3,6 +3,7 @@ package com.example.testgit;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello git2");
+        System.out.println("master second git");
     }
 }
 //
