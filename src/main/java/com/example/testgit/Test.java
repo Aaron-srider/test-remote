c150fe2e7a9fa848bb4f729e34c07dbf42d9e4a8
chaowen <75847511+Aaron-srider@users.noreply.github.com>
2021-08-07 18:11:42 +0800

    remote commit

--- src/main/java/com/example/testgit/Test.java
+++ src/main/java/com/example/testgit/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git2");
         System.out.println("master second git");
-        System.out.println("login git1");
+        System.out.println("login git");
     }
 }
 //
