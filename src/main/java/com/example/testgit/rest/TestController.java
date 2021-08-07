d278707b8c2f659cbf27afecbda6a426382df430
wc <mygit@bistu.cn>
2021-08-07 22:59:15 +0800

    master add Dao UserDao fix API rest.TestController.listUser

--- src/main/java/com/example/testgit/rest/TestController.java
+++ src/main/java/com/example/testgit/rest/TestController.java
@@ -1,7 +1,10 @@
 package com.example.testgit.rest;
 
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.testgit.dao.UserDao;
 import com.example.testgit.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +14,20 @@ import java.util.List;
 @RestController
 public class TestController {
 
+    @Autowired
+    UserDao userDao;
+
     @GetMapping("/listUser")
     public Object listUser() {
-        List<User> list = new ArrayList<User>();
-        list.add(new User("zx", 11));
-        list.add(new User("ls", 32));
-        list.add(new User("ww", 20));
-        return list;
+
+
+        List<User> users = userDao.selectList(null);
+
+
+        //List<User> list = new ArrayList<User>();
+        //list.add(new User("zx", 11));
+        //list.add(new User("ls", 32));
+        //list.add(new User("ww", 20));
+        return users;
     }
 }
